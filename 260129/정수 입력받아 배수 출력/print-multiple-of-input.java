471bac107911130ca2c4817c4b436bbb92683ada
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-01-29 13:30:35 +0900

    Title: 정수 입력받아 배수 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260129/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(result * i + " ");
+        }
+
+    }
+}
