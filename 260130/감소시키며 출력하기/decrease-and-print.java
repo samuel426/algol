8903d796ba54f61cdac1c741f80ca9497c199834
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-01-30 07:20:32 +0900

    Title: 감소시키며 출력하기, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260130/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        for(int i = 17; i >= 5; i--) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
