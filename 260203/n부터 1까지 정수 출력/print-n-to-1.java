758652d5e7b0eb11b33551ea9540080c51876273
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-02-03 06:39:42 +0900

    Title: n부터 1까지 정수 출력, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260203/n부터 1까지 정수 출력/print-n-to-1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int start = 1;
+        while(start <= N) {
+            System.out.print(start + " ");
+            N -= 1;
+        }
+
+    }
+}
