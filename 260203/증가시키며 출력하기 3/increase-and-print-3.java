d61331b9e6b96812d6f67505e63afed1b6ebbd3b
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-02-03 06:28:48 +0900

    Title: 증가시키며 출력하기 3, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260203/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+
+        int start = 10;
+        while(start != 28) {
+            System.out.print(start + " ");
+            start += 2;
+        }
+
+    }
+}
