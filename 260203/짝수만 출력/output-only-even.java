b33b002d4449ec135519ebfb0fb3d1a417e69f24
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-02-03 06:34:25 +0900

    Title: 짝수만 출력, Time: 94ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260203/짝수만 출력/output-only-even.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[0]);
+
+        while(a != b) {
+            
+            if(a % 2 == 0) {
+                System.out.print(a + " ");
+            }
+            a++;
+        }
+
+    }
+}
