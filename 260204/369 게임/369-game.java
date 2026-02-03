c747707c2ba8db11baf1896b6cdaf3767bbd7e24
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-02-04 07:14:10 +0900

    Title: 369 게임, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260204/369 게임/369-game.java
+++ 260204/369 게임/369-game.java
@@ -10,14 +10,12 @@ public class Main {
 
         for(int i = 1; i <= a; i++) {
             String num = Integer.toString(i);
-            if(num.contains("3") || num.contains("3") || num.contains("9")) {
+            if(num.contains("3") || num.contains("6") || num.contains("9")) {
                 System.out.print("0 ");
                 continue;
             }
             System.out.print(i + " ");
         }
-
-
 
     }
 }
