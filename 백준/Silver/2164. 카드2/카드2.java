898902c31cfc6a7edf4c88dcc8256e8cc7e03d8f
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-02-09 15:45:05 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 45716 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i = 0; i < input; i++) {
+			queue.offer(i + 1);
+		}
+		
+		while(queue.size() > 1) {
+			queue.poll();
+			queue.offer(queue.poll());
+		}
+		System.out.println(queue.poll());
+	}
+}
