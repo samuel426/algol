4682ac2d0e38d32f00feba405fa730b367479508
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-02-23 23:17:22 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] stair = new int[n + 1];
+        int[] dp = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            stair[i] = Integer.parseInt(br.readLine());
+        }
+        
+        if (n == 1) {
+            System.out.println(stair[1]);
+            return;
+        }
+        
+        dp[1] = stair[1];
+        dp[2] = stair[1] + stair[2];
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(
+                dp[i - 2] + stair[i],
+                dp[i - 3] + stair[i - 1] + stair[i]
+            );
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
