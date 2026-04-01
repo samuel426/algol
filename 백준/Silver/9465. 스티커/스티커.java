2498907438c75be9e0747c8e2c4087ff207bc8fa
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-04-01 10:46:25 +0900

    [Silver I] Title: 스티커, Time: 712 ms, Memory: 119828 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -3,52 +3,51 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
-
-		while (T-- > 0) {
-			int n = Integer.parseInt(br.readLine());
-
-			int[] top = new int[n];
-			int[] bottom = new int[n];
-
+		
+		for(int t = 0; t < T; t++) {
+			int sc = Integer.parseInt(br.readLine());
+			
+			int[] line1 = new int[sc];
+			int[] line2 = new int[sc];
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < n; i++) {
-				top[i] = Integer.parseInt(st.nextToken());
+			for(int i = 0; i < sc; i++) {
+				line1[i] = Integer.parseInt(st.nextToken());
 			}
-
+			
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < n; i++) {
-				bottom[i] = Integer.parseInt(st.nextToken());
+			for(int i = 0; i < sc; i++) {
+				line2[i] = Integer.parseInt(st.nextToken());
 			}
-
-			if (n == 1) {
-				sb.append(Math.max(top[0], bottom[0])).append('\n');
+			
+			if(sc == 1) {
+				sb.append(Math.max(line1[0], line2[0])).append("\n");
 				continue;
 			}
+			
+			int[][] dp = new int[2][sc];
 
-			int topPrev2 = top[0];
-			int bottomPrev2 = bottom[0];
-
-			int topPrev1 = bottom[0] + top[1];
-			int bottomPrev1 = top[0] + bottom[1];
-
-			for (int i = 2; i < n; i++) {
-				int topCur = Math.max(bottomPrev1, bottomPrev2) + top[i];
-				int bottomCur = Math.max(topPrev1, topPrev2) + bottom[i];
-
-				topPrev2 = topPrev1;
-				bottomPrev2 = bottomPrev1;
-				topPrev1 = topCur;
-				bottomPrev1 = bottomCur;
+			dp[0][0] = line1[0];
+			dp[1][0] = line2[0];
+			
+			dp[0][1] = dp[1][0] + line1[1];
+			dp[1][1] = dp[0][0] + line2[1];
+			
+			for(int i = 2; i < sc; i++) {
+				dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + line1[i];
+				dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + line2[i];
 			}
-
-			sb.append(Math.max(topPrev1, bottomPrev1)).append('\n');
+			
+			int answer = Math.max(dp[0][sc - 1], dp[1][sc - 1]);
+			sb.append(answer).append("\n");
+			
 		}
-
-		System.out.print(sb);
+		System.out.println(sb);
+		
 	}
 }
