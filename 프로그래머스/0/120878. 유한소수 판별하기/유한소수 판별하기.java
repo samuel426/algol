b5d59f19ff766d673754578e4d87a034bc997afc
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-04-21 21:09:48 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int a, int b) {
+        int div = gcd(a, b);
+        b /= div;
+        while(b % 2 == 0) {
+            b /= 2;
+        }
+        while(b % 5 == 0) {
+            b /= 5;
+        }
+        
+        return b == 1 ? 1 : 2;
+    }
+    
+    public static int gcd(int a, int b) {
+        
+        while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        
+        return a;
+    }
+}
