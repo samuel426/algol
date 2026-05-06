61adb1da05b80c8e161bbb6e576c566d261ad09e
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-05-06 09:24:27 +0900

    [Passed] 사각형 채우기 2 (90ms, 9MB)

---
+++ 260506/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static int MAX_N = 1002;
+
+    public static void main(String[] args) throws Exception{
+        int[] dp = new int[MAX_N];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        dp[1] = 1;
+        dp[2] = 3;
+
+        
+
+        for(int i = 3; i <= n; i++)
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+
+        System.out.println(dp[n]);
+
+    }
+}
