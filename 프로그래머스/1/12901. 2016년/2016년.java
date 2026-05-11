48985d12a0f77db51606dd9725aaa0f9ce90b934
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-05-11 10:42:56 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,6 +1,16 @@
 class Solution {
     public String solution(int a, int b) {
         String answer = "";
+        int[] mon = new int[] {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+        String[] re = new String[] {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        for(int i = 1; i < a; i++) {
+            b += mon[i - 1];
+        }
+        b = (b - 1) % 7;
+        answer = re[b];
+            
+            
+            
         return answer;
     }
 }
