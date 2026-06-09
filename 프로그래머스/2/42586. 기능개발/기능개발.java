57d68cf99c8019428f09fc651b4015e96375f667
Sunghyun Woo <samuel.woo2014@gmail.com>
2026-06-09 10:05:26 +0900

    [level 2] Title: 기능개발, Time: 3.56 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for(int i = 0; i < progresses.length; i++) {
+            float p = progresses[i];
+            float s = speeds[i];
+            int days = (int)Math.ceil((100 - p) / s);
+            queue.offer(days);
+        }
+        Queue<Integer> answer = new LinkedList<>();
+        int d = queue.poll();
+        int count = 1;
+        while(!queue.isEmpty()) {
+            int n = queue.poll();
+            if(d >= n) {
+                count++;
+                continue;
+            }
+            answer.offer(count);
+            count = 1;
+            d = n;
+        }
+        answer.offer(count);
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
